167dedb51e6da4e219e660b161cb126574d0c2cf
KEEN <homurahomu@naver.com>
2021-10-26 14:20:44 +0900

    configure UI and init code

--- PracticeHideAPIKEY/WeatherViewController.swift
+++ PracticeHideAPIKEY/WeatherViewController.swift
@@ -11,10 +11,31 @@ import SwiftyJSON
 
 class WeatherViewController: UIViewController {
 
+  // MARK: - UI
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var currentTempLabel: UILabel!
+  @IBOutlet weak var currentHumiditylabel: UILabel!
+  
+  // MARK: - View Life-Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    fetchWeatherInfomation()
   }
-
+  
+  // MARK: Fetching Datas
+  func fetchWeatherInfomation() {
+    let url = "url을 적어주세요"
+    
+    AF.request(url, method: .get).validate().responseJSON { response in
+      switch response.result {
+      case .success(let value):
+        let json = JSON(value)
+        print("JSON: \(json)")
+        
+      case .failure(let error):
+        print(error)
+      }
+    }
+  }
 }
 
