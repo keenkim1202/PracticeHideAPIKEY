a7e4c12e31cea4951a47f987eb77874716db4752
KEEN <homurahomu@naver.com>
2021-10-26 14:12:22 +0900

    init  commit

---
+++ PracticeHideAPIKEY/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherViewController.swift
+//  PracticeHideAPIKEY
+//
+//  Created by KEEN on 2021/10/26.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class WeatherViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# PracticeHideAPIKEY
+github에 APIKEY가 올라가지 않도록 설정하는 법을 연습한 레포 입니다.
